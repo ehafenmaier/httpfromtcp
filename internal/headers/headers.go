455ad6d45343215c56f5ec67fa6b763b8bf6cc87
Erich Hafenmaier <ehafenmaier@gmail.com>
2025-09-01 17:04:56 -0700

    Add alphanumeric and special character validation for header key

--- internal/headers/headers.go
+++ internal/headers/headers.go
@@ -3,6 +3,7 @@ package headers
 import (
 	"bytes"
 	"fmt"
+	"regexp"
 	"strings"
 )
 
@@ -30,20 +31,44 @@ func (h Headers) Parse(data []byte) (n int, done bool, err error) {
 		}
 
 		// Parse the header line
-		line := string(data[bytesConsumed : bytesConsumed+idx])
-		parts := strings.SplitN(line, ":", 2)
-
-		// If there is any whitespace at the end of the key the header line is invalid
-		if strings.TrimRight(parts[0], " ") != parts[0] {
-			return 0, false, fmt.Errorf("invalid header line: %s", line)
+		key, value, err := parseHeaderLine(data[bytesConsumed : bytesConsumed+idx])
+		if err != nil {
+			return 0, false, err
 		}
 
 		// Add the key value pair to the headers map
-		key := strings.TrimSpace(parts[0])
-		value := strings.TrimSpace(parts[1])
 		h[key] = value
 
 		// Increment the number of bytes consumed
 		bytesConsumed += idx + len(rn)
 	}
+}
+
+func parseHeaderLine(data []byte) (string, string, error) {
+	line := string(data)
+	parts := strings.SplitN(line, ":", 2)
+
+	// If we don't have exactly two parts the header line is invalid
+	if len(parts) != 2 {
+		return "", "", fmt.Errorf("invalid header line: %s", line)
+	}
+
+	// If there is any whitespace at the end of the key the header line is invalid
+	if strings.TrimRight(parts[0], " ") != parts[0] {
+		return "", "", fmt.Errorf("invalid header line: %s", line)
+	}
+
+	// If the header key doesn't only have alphanumeric and certain special
+	// characters it is invalid
+	pattern := "^[a-zA-Z0-9!#$%&'*+-.^_`|~]+$"
+	re := regexp.MustCompile(pattern)
+	if !re.MatchString(strings.TrimSpace(parts[0])) {
+		return "", "", fmt.Errorf("invalid header key: %s", parts[0])
+	}
+
+	// Header line is valid so return lowercased key and value
+	key := strings.ToLower(strings.TrimSpace(parts[0]))
+	value := strings.TrimSpace(parts[1])
+
+	return key, value, nil
 }
--- internal/headers/headers_test.go
+++ internal/headers/headers_test.go
@@ -14,7 +14,7 @@ func TestHeadersParse(t *testing.T) {
 	n, done, err := headers.Parse(data)
 	require.NoError(t, err)
 	require.NotNil(t, headers)
-	assert.Equal(t, "localhost:42069", headers["Host"])
+	assert.Equal(t, "localhost:42069", headers["host"])
 	assert.Equal(t, 23, n)
 	assert.True(t, done)
 
@@ -24,7 +24,7 @@ func TestHeadersParse(t *testing.T) {
 	n, done, err = headers.Parse(data)
 	require.NoError(t, err)
 	require.NotNil(t, headers)
-	assert.Equal(t, "localhost:12345", headers["Host"])
+	assert.Equal(t, "localhost:12345", headers["host"])
 	assert.Equal(t, 31, n)
 	assert.True(t, done)
 
@@ -34,13 +34,21 @@ func TestHeadersParse(t *testing.T) {
 	n, done, err = headers.Parse(data)
 	require.NoError(t, err)
 	require.NotNil(t, headers)
-	assert.Equal(t, "localhost:12345", headers["Host"])
-	assert.Equal(t, "test-agent", headers["User-Agent"])
+	assert.Equal(t, "localhost:12345", headers["host"])
+	assert.Equal(t, "test-agent", headers["user-agent"])
 	assert.Equal(t, 61, n)
 
 	// Test: Invalid spacing header
 	headers = NewHeaders()
 	data = []byte("       Host  : localhost:42069       \r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.Error(t, err)
+	assert.Equal(t, 0, n)
+	assert.False(t, done)
+
+	// Test: Invalid characters in header field name
+	headers = NewHeaders()
+	data = []byte("    H@st: localhost:42069    \r\n\r\n")
 	n, done, err = headers.Parse(data)
 	require.Error(t, err)
 	assert.Equal(t, 0, n)
